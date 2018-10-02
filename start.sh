0222cc4fb1b08d3869c9596fb323d5ca82ba7a3a
Alastair Porter <alastair.porter@upf.edu>
2018-10-02 11:36:52 +0200

    Don't change user uid if the variable is set but empty

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ if [[ $(id -u) != 0 ]]; then
     exit 1
 fi
 
-if [[ -v JUPYTER_USER_ID && $JUPYTER_USER_ID != 0 ]]; then
+if [[ -v JUPYTER_USER_ID && $JUPYTER_USER_ID != 0 && -n $JUPYTER_USER_ID ]]; then
     echo Set mir UID to: $JUPYTER_USER_ID
     usermod -u $JUPYTER_USER_ID mir
 
